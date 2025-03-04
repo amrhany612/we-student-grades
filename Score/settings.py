b171bd4254eb52f3a8fe53c2cca66399556858f5
amr hany <amrh18039@gmail.com>
2025-03-04 21:51:31 +0200

    third

--- Score/settings.py
+++ Score/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9)i&#njncila72c=%frpsjijfw(35=txeba=un1j-ltua2qs&7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['http://we-student-grades.vercel.app/','*']
 
 
 # Application definition
